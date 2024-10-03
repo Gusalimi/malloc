2c80536a30ab3d4bf33cd67d7f46e25c889d2be2
Guillaume Sailé <saileguillaume2@gmail.com>
2024-10-03 14:55:11 +0200

    [Free] Update TODO

--- src/free.c
+++ src/free.c
@@ -6,11 +6,15 @@
 /*   By: gsaile <gsaile@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 11:02:05 by gsaile            #+#    #+#             */
-/*   Updated: 2024/10/03 14:50:41 by gsaile           ###   ########.fr       */
+/*   Updated: 2024/10/03 14:54:49 by gsaile           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/malloc.h"
+
+// TODO:
+// - Check if double free
+// - Munmap if empty heap
 
 void free(void *ptr) {
 	((t_block *)((char *)ptr - sizeof(t_block)))->freed = TRUE;
